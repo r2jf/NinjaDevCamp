ddb9f2924dbe1d955d4747ea190f3b160e664903
Jose Ramirez <r2jf@hotmail.com>
2013-11-16 02:23:08 -0600

    6

---
+++ ff2/page.js
@@ -0,0 +1,61 @@
+$(function() {
+    var client = new WindowsAzure.MobileServiceClient('https://ninjadevcamp.azure-mobile.net/', 'sJJgZmkWRPBfpwyZomSFJCWYwIakDs43'),
+        todoItemTable = client.getTable('ninja2');
+
+    // Read current data and rebuild UI.
+    // If you plan to generate complex UIs like this, consider using a JavaScript templating library.
+    function refreshTodoItems() {
+        var query = todoItemTable.where({ mostrar: true });
+
+        query.read().then(function(todoItems) {
+            var listItems = $.map(todoItems, function(item) {
+                return $('<li>')
+                    .attr('data-todoitem-id', item.id)
+                    .append($('<input type="checkbox" class="item-complete">').prop('checked', item.mostrar))
+                    .append($('<div>').append($('<input class="item-text">').val(item.ciudad + ', ' + item.ff)));
+            });
+
+            $('#todo-items').empty().append(listItems).toggle(listItems.length > 0);
+            $('#summary').html('<strong>' + todoItems.length + '</strong> item(s)');
+        }, handleError);
+    }
+
+    function handleError(error) {
+        var text = error + (error.request ? ' - ' + error.request.status : '');
+        $('#errorlog').append($('<li>').text(text));
+    }
+
+    function getTodoItemId(formElement) {
+        return Number($(formElement).closest('li').attr('data-todoitem-id'));
+    }
+
+    // Handle insert
+    $('#add-item').submit(function(evt) {
+        var textbox = $('#new-item-text'),
+            itemText = textbox.val();
+        if (itemText !== '') {
+            todoItemTable.insert({ ciudad: itemText, mostrar: true }).then(refreshTodoItems, handleError);
+        }
+        textbox.val('').focus();
+        evt.preventDefault();
+    });
+
+    // Handle update
+    $(document.body).on('change', '.item-text', function() {
+        var newText = $(this).val();
+        todoItemTable.update({ id: getTodoItemId(this), ciudad: newText }).then(null, handleError);
+    });
+
+    $(document.body).on('change', '.item-complete', function() {
+        var isComplete = $(this).prop('checked');
+        todoItemTable.update({ id: getTodoItemId(this), mostrar: isComplete }).then(refreshTodoItems, handleError);
+    });
+
+    // Handle delete
+    $(document.body).on('click', '.item-delete', function () {
+        todoItemTable.del({ id: getTodoItemId(this) }).then(refreshTodoItems, handleError);
+    });
+
+    // On initial load, start by fetching the current data
+    refreshTodoItems();
+});
